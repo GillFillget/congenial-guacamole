214ad25e2a53fb19e22ce4107a350f40ca9dc1d8
Austin Emberlin <87716672+WAEmberlin@users.noreply.github.com>
2023-04-11 17:25:46 -0500

    Frontend_Responsiveness_Update

--- claycounty/src/Footer.js
+++ claycounty/src/Footer.js
@@ -5,12 +5,11 @@ const Footer = () => {
     return (
         <div>
         <div class="social-container">
-      <h3><u>Social Media</u></h3>
-      <a href="https://www.facebook.com/ccksmuseum/"
-        className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-    </div>  
+          <h3><u>Social Media</u></h3>
+          <a href="https://www.facebook.com/ccksmuseum/" className="facebook social">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+        </div>  
         <div className="footer">
             <p>Hours: Tues - Sun. 1-5 pm</p>
             <p>(785) 632-3786 </p>
