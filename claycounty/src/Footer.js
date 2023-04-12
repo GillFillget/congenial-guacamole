aa7392a44b40a9a838ad915d0a6dab463131c340
Austin Emberlin <87716672+WAEmberlin@users.noreply.github.com>
2023-04-11 20:07:52 -0500

    Footer changes

--- claycounty/src/Footer.js
+++ claycounty/src/Footer.js
@@ -3,17 +3,22 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer-container">
         <div class="social-container">
           <h3><u>Social Media</u></h3>
           <a href="https://www.facebook.com/ccksmuseum/" className="facebook social">
             <FontAwesomeIcon icon={faFacebook} size="2x" />
           </a>
-        </div>  
-        <div className="footer">
+          </div>  
+        <div>
             <p>Hours: Tues - Sun. 1-5 pm</p>
             <p>(785) 632-3786 </p>
             <p>ccmuseum@twinvalley.net</p>
+        </div>
+        <div>
+            <p>Our Address:</p>
+            <p>518 Lincoln Ave</p>
+            <p>Clay Center, KS 67432</p>
         </div>
         </div>
      );
